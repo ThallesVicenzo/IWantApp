a4bf76ad99b59a1d18484ea44df4a3e39766cbef
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-16 15:35:46 -0300

    feat: adds entity

---
+++ Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+    public required string  Name { get; set; }
+    public required string CreatedBy { get; set; }
+    public required DateTime CreatedOn { get; set; }
+    public required DateTime EditedOn { get; set; }
+    public required string EditedBy { get; set; }
+}
