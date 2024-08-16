3b5e5b554e318d46e5c188284c4dfeb425a64a5a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-16 16:02:56 -0300

    feat: adds ApplicationDbContext

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace IWantApp.Infra.Data;
+
+using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().Property(propertyExpression => propertyExpression.Name).IsRequired();
+        modelBuilder.Entity<Product>().Property(propertyExpression => propertyExpression.Description).HasMaxLength(255);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<string>().HaveMaxLength(100);
+        base.ConfigureConventions(configurationBuilder);
+    }
+}
