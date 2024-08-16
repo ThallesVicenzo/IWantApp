00b6280b1345627e3300f9042a523feef5c057a7
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-16 16:02:47 -0300

    feat: adds category id field

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,6 +3,7 @@
 public class Product : Entity
 {
     public required Category category { get; set; }
+    public required int CategoryId { get; set; }
     public required string Description { get; set; }
     public required bool HasStock { get; set; }
 
