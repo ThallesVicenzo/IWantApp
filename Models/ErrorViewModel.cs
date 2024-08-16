336d65d1e84ed9712bb8824a70feb9f288013bc6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-16 16:02:34 -0300

    removes unused folders

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IWantApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
