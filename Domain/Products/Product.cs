7f54e65134ed3198211aebff25b4e2d840efba35
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-19 14:57:56 -0300

    refactor: adds isActive field & changes categoryId type

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,8 +3,9 @@
 public class Product : Entity
 {
     public required Category category { get; set; }
-    public required int CategoryId { get; set; }
+    public required Guid CategoryId { get; set; }
     public required string Description { get; set; }
     public required bool HasStock { get; set; }
+    public bool isActive { get; set; } = true;
 
 }
