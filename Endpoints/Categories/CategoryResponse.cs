43e8e6995793b84d008cd6a25fe5179062523c85
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-19 15:08:45 -0300

    feat: adds CategoryResponse

---
+++ Endpoints/Categories/CategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace IWantApp.Endpoints.Categories;
+
+public class CategoryResponse
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required bool Active { get; set; }
+};
