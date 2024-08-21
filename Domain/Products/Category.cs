f88429f09fa06377b8196f103da5e67a6c4a6633
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-21 15:58:23 -0300

    refactor: validades category entity fields & initiates by constructor

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -1,6 +1,26 @@
-﻿namespace IWantApp.Domain.Products;
+﻿using Flunt.Validations;
+
+namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public bool IsActive { get; set; } = true;
+    public bool IsActive { get; set; }
+
+    public Category(string name, string createdBy, string editedBy)
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name")
+            .IsNotNullOrEmpty(createdBy, "createdBy")
+            .IsNotNullOrEmpty(editedBy, "editedBy");
+
+        AddNotifications(contract);
+
+        Name = name;
+        IsActive = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        editedBy = "Test";
+        EditedOn = DateTime.Now;
+    }
 }
