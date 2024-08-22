ce762e2c07ba7d4e3c46fbb2045b744cc4dbde53
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-22 14:36:44 -0300

    feat: adds edit info & Validate function

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -4,24 +4,37 @@ namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public bool IsActive { get; set; }
+    public bool IsActive { get; private set; }
+    public string Name { get; private set; }
 
     public Category(string name, string createdBy, string editedBy)
     {
-        var contract = new Contract<Category>()
-            .IsNotNullOrEmpty(name, "Name")
-            .IsGreaterOrEqualsThan(name, 3, "Name")
-            .IsNotNullOrEmpty(createdBy, "createdBy")
-            .IsNotNullOrEmpty(editedBy, "editedBy");
-
-        AddNotifications(contract);
-
         Name = name;
         IsActive = true;
         CreatedBy = createdBy;
         EditedBy = editedBy;
         CreatedOn = DateTime.Now;
-        editedBy = "Test";
         EditedOn = DateTime.Now;
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Category>()
+                    .IsNotNullOrEmpty(Name, "Name")
+                    .IsGreaterOrEqualsThan(Name, 3, "Name")
+                    .IsNotNullOrEmpty(CreatedBy, "CreatedBy")
+                    .IsNotNullOrEmpty(EditedBy, "EditedBy");
+
+        AddNotifications(contract);
+    }
+
+    public void EditInfo(string name, bool active)
+    {
+        IsActive = active;
+        Name = name;
+
+        Validate();
     }
 }
