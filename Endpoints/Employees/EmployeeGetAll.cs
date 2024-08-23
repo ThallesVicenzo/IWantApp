435d6c501e053a5ba48415384009ceda5371de9f
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-23 16:11:14 -0300

    feat: applies dapper query & common error handlers

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -10,18 +12,35 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
     {
-        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-        var employees = new List<EmployeeResponse>();
+        if (page == null)
+            return Results.NotFound("Page can not be null!");
+        if (page <= 0)
+            return Results.BadRequest("Page value can not be less than 1");
+        if (rows == null)
+            return Results.NotFound("Rows can not be null!");
+        if (rows > 10)
+            return Results.BadRequest("Rows value can not be more than 10");
 
-        foreach (var user in users)
-        {
-            var claims = userManager.GetClaimsAsync(user).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null ? claimName.Value : string.Empty;
-            employees.Add(new EmployeeResponse(user.Email!, userName));
-        }
+
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+
+        var query = @"select Email, ClaimValue as Name
+                from AspNetUsers u inner
+                join AspNetUserClaims c
+                on u.id = c.UserId and claimtype = 'Name'
+                order by name
+                OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+        var employees = db.Query<EmployeeResponse>(
+            query,
+            new
+            {
+                page,
+                rows,
+            }
+            );
 
         return Results.Ok(employees);
     }
