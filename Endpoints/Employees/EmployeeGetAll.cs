35492e88bbb7cdf04b4ac33426b6fdaedadb5200
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-23 16:24:59 -0300

    refactor: updates endpoint errors & query

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -1,5 +1,4 @@
-﻿using Dapper;
-using Microsoft.Data.SqlClient;
+﻿using IWantApp.Infra.Data;
 
 
 namespace IWantApp.Endpoints.Employees;
@@ -12,36 +11,17 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int? page, int? rows, IConfiguration configuration)
+    public static IResult Action(int? page, int? rows, QueryAllUsersWithClaimName query)
     {
         if (page == null)
-            return Results.NotFound("Page can not be null!");
+            return Results.BadRequest("Page can not be null!");
         if (page <= 0)
             return Results.BadRequest("Page value can not be less than 1");
         if (rows == null)
-            return Results.NotFound("Rows can not be null!");
+            return Results.BadRequest("Rows can not be null!");
         if (rows > 10)
             return Results.BadRequest("Rows value can not be more than 10");
 
-
-        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
-
-        var query = @"select Email, ClaimValue as Name
-                from AspNetUsers u inner
-                join AspNetUserClaims c
-                on u.id = c.UserId and claimtype = 'Name'
-                order by name
-                OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
-
-        var employees = db.Query<EmployeeResponse>(
-            query,
-            new
-            {
-                page,
-                rows,
-            }
-            );
-
-        return Results.Ok(employees);
+        return Results.Ok(query.Execute(page.Value, rows.Value));
     }
 }
