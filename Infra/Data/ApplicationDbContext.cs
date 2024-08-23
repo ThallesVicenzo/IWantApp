5f6417eb877e9c77a3e772fb5772b3afa62b5f7b
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-23 10:44:24 -0300

    feature: applies identity on project

--- Infra/Data/ApplicationDbContext.cs
+++ Infra/Data/ApplicationDbContext.cs
@@ -2,9 +2,11 @@
 
 using Flunt.Notifications;
 using IWantApp.Domain.Products;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
