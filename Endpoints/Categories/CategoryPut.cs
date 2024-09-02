82d371bf09c70de314bd5a6a1631d368bd58e270
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-02 14:56:18 -0300

    feat: sets claim values from token on CategoryPut

--- Endpoints/Categories/CategoryPut.cs
+++ Endpoints/Categories/CategoryPut.cs
@@ -1,5 +1,6 @@
 ﻿using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Categories;
 
@@ -9,14 +10,16 @@ public class CategoryPut
     public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action([FromRoute] Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
+    public static IResult Action([FromRoute] Guid id, HttpContext http, CategoryRequest categoryRequest, ApplicationDbContext context)
     {
+        var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
         var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
 
         if (category == null)
             return Results.NotFound("Category not found!");
 
-        category.EditInfo(categoryRequest.Name, categoryRequest.Active);
+        category.EditInfo(categoryRequest.Name, categoryRequest.Active, userId);
 
         if (!category.IsValid)
             return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
