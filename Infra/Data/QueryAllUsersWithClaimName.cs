cde5815bcc399c00c254a84ab208f7f18418e20b
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-02 16:07:13 -0300

    refactor: changes QueryAllUsersWithClaimName to use async & await

--- Infra/Data/QueryAllUsersWithClaimName.cs
+++ Infra/Data/QueryAllUsersWithClaimName.cs
@@ -13,7 +13,7 @@ public class QueryAllUsersWithClaimName
         this.configuration = configuration;
     }
 
-    public IEnumerable<EmployeeResponse> Execute(int page, int rows)
+    public async Task<IEnumerable<EmployeeResponse>> Execute(int page, int rows)
     {
         var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
 
@@ -24,7 +24,7 @@ public class QueryAllUsersWithClaimName
                 order by name
                 OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
 
-        return db.Query<EmployeeResponse>(
+        return await db.QueryAsync<EmployeeResponse>(
             query,
             new
             {
