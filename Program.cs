fea1f51581bc2a25f3328851c48339f6de8f0eda
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-03 15:02:44 -0300

    feat: adds uneplemented error handler

--- Program.cs
+++ Program.cs
@@ -58,4 +58,19 @@ app.MapMethods(EmployeeGetAll.Template, EmployeeGetAll.Methods, EmployeeGetAll.H
 
 app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
 
+app.UseExceptionHandler("/error");
+app.Map("/error", (HttpContext http) =>
+{
+    var error = http.Features?.Get<IExceptionHandlerFeature>()?.Error;
+
+    if (error != null)
+    {
+        if (error is SqlException)
+            return Results.Problem(title: "Database out", statusCode: 500);
+    }
+
+    return Results.Problem(title: "An error ocurred.", statusCode: 500);
+});
+
+
 app.Run();
