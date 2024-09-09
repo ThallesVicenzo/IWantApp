7864b1b5fcabb1c3283e85813e7bf2ee746f7a05
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-09 15:49:04 -0300

    feat: adds filter options

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -8,9 +8,26 @@ public class ProductGetShowcase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static IResult Action(ApplicationDbContext context)
+    public static IResult Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
     {
-        var products = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category!.IsActive).OrderBy(p => p.Name).ToList();
+        if (page == null)
+            page = 1;
+        if (row == null)
+            row = 10;
+        if (string.IsNullOrEmpty(orderBy))
+            orderBy = "Placeholder";
+
+        var queryBase = context.Products.Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.IsActive);
+
+        if (orderBy == "Placeholder")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else
+            queryBase = queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+        var products = queryFilter.ToList();
 
         var results = products.Select(p => new ProductResponse(p.Name!, p.Category!.Name, p.Description!, p.HasStock, p.Price, p.IsActive));
 
