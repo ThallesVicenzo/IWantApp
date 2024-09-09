b5997741762352aed0c7ca27db020292b5f8b24b
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-09 15:30:53 -0300

    feat: adds product get show case end point

---
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -0,0 +1,19 @@
+﻿
+namespace IWantApp.Endpoints.Products;
+
+public class ProductGetShowcase
+{
+    public static string Template => "/products/showcase";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    [AllowAnonymous]
+    public static IResult Action(ApplicationDbContext context)
+    {
+        var products = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category!.IsActive).OrderBy(p => p.Name).ToList();
+
+        var results = products.Select(p => new ProductResponse(p.Name!, p.Category!.Name, p.Description!, p.HasStock, p.Price, p.IsActive));
+
+        return Results.Ok(results);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -62,6 +62,7 @@ app.MapMethods(EmployeeGetAll.Template, EmployeeGetAll.Methods, EmployeeGetAll.H
 app.MapMethods(ProductPost.Template, ProductPost.Methods, ProductPost.Handle);
 app.MapMethods(ProductGetAll.Template, ProductGetAll.Methods, ProductGetAll.Handle);
 app.MapMethods(ProductGetById.Template, ProductGetById.Methods, ProductGetById.Handle);
+app.MapMethods(ProductGetShowcase.Template, ProductGetShowcase.Methods, ProductGetShowcase.Handle);
 
 app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
 
