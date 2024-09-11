5bcbb263a822964505729b025ea2c7681f10e9f2
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-11 16:02:21 -0300

    feat: adds CpfPolicy

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddAuthorization(options =>
     .Build();
 
     options.AddPolicy("EmployeePolicy", p => p.RequireAuthenticatedUser().RequireClaim("EmployeeCode"));
+    options.AddPolicy("CpfPolicy", p => p.RequireAuthenticatedUser().RequireClaim("Cpf"));
 });
 
 builder.Services.AddAuthentication(x =>
