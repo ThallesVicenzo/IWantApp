14f4d5b5442351aa4fece282f85eb318f2463d08
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-11 14:43:56 -0300

    feat: updates AppDBContext with Order migrations

--- Infra/Data/ApplicationDbContext.cs
+++ Infra/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿namespace IWantApp.Infra.Data;
+﻿using IWantApp.Domain.Orders;
+
+namespace IWantApp.Infra.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -16,6 +19,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(10,2)").IsRequired();
 
         modelBuilder.Entity<Category>().Property(propertyExpression => propertyExpression.Name).IsRequired();
+
+        modelBuilder.Entity<Order>().Property(o => o.ClientId).IsRequired();
+        modelBuilder.Entity<Order>().Property(o => o.DeliveryAddress).IsRequired();
+        modelBuilder.Entity<Order>().HasMany(o => o.Products).WithMany(p => p.Orders).UsingEntity(x => x.ToTable("OrderProducts"));
+
     }
 
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
