4aa0c57ea563d60b8c1d3f68758bc7011ff607f8
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-11 16:02:11 -0300

    fix: fixes small code problem

--- Endpoints/Products/ProductRequest.cs
+++ Endpoints/Products/ProductRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace IWantApp.Endpoints.Products;
 
-public record ProductRequest(Guid CategoryId, string Name, string Description, decimal price, bool HasStock, bool Active);
+public record ProductRequest(Guid CategoryId, string Name, string Description, decimal Price, bool HasStock, bool Active);
