50552c98cd3f178f3d075670ca4acf409392c56c
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-16 12:27:47 -0300

    feat: adds OrderResponse

---
+++ Endpoints/Orders/OrderResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace IWantApp.Endpoints.Orders;
+
+public record OrderResponse(Guid ProductId, string ClientEmail, string DeliveryAddress, IEnumerable<OrderProduct> Products);
+
+public record OrderProduct(Guid id, string Name);
