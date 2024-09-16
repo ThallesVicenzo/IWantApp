d5c3ea2cb7946a5a308b4eb2d6cac755f4243299
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-16 15:08:39 -0300

    feat: adds MostOrderedGet endpoint

---
+++ Endpoints/Products/MostOrderedGet.cs
@@ -0,0 +1,16 @@
+﻿namespace IWantApp.Endpoints.Products;
+
+public class MostOrderedGet
+{
+    public static string Template => "/products/most-ordered";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    [Authorize(Policy = "EmployeePolicy")]
+    public static async Task<IResult> Action(QueryMostOrderedProducts query)
+    {
+        var result = await query.Execute();
+
+        return Results.Ok(result);
+    }
+}
