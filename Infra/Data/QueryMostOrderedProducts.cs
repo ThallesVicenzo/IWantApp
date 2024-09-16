b1e38a615826c61d37747eb1e64617d54fe587fb
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-09-16 15:09:01 -0300

    feat: adds QueryMostOrderedProducts query

---
+++ Infra/Data/QueryMostOrderedProducts.cs
@@ -0,0 +1,33 @@
+﻿namespace IWantApp.Infra.Data;
+
+public class QueryMostOrderedProducts
+{
+    private readonly IConfiguration configuration;
+
+    public QueryMostOrderedProducts(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public async Task<IEnumerable<ProductsSoldResponse>> Execute()
+    {
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+
+        string query = @"
+                SELECT
+                    p.Id,
+                    p.Name,
+                    COUNT(*) AS Amount
+                FROM
+                    Orders o
+                INNER JOIN OrderProducts op ON o.Id = op.OrdersId
+                INNER JOIN Products p ON p.Id = op.ProductsId
+                GROUP BY
+                    p.Id, p.Name
+                ORDER BY Amount DESC
+                ";
+
+        return await db.QueryAsync<ProductsSoldResponse>(query);
+    }
+
+}
